02e474aedf8e495eda18cc5dc31ecd8d92b805e4
F4BI4NFF <fabianff95@hotmail.com>
2017-10-25 23:27:03 -0300

    Distrito y clase Titan

--- DS/Servidor/src/DS/Distrit.java
+++ DS/Servidor/src/DS/Distrit.java
@@ -1,8 +1,11 @@
 package DS;
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
+import java.util.Scanner;
 
 public class Distrit {
     private String DistritName;
@@ -10,6 +13,7 @@ public class Distrit {
     private int MultiCastPort;
     private String RequestIP;
     private int ResquestPort;
+    List<String> lista_titanes;
 
     public String getDistritName() {
         return DistritName;
@@ -53,17 +57,30 @@ public class Distrit {
 
     public static void main(String[] args){
         Server servidor = new Server();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("[Distrito] Ingresar Puerto de Trabajo Distrito");
+        servidor.setListenPort(scanner.nextInt());
+        scanner.close();
 
         try{
+
             ServerSocket listenSocket = new ServerSocket(servidor.getListenPort());
-            System.out.println("Server Central en espera...");
+            System.out.println("Distrito en espera...");
             while (true) {
                 Socket cs = listenSocket.accept();
                 System.out.println("Nueva conexion entrante: ");
                 System.out.println("Puerto: " + servidor.getMultiCastPort());
+                Scanner scanner2 = new Scanner(System.in);
+                System.out.println("aaaaaaaaaaaaaa");
+                System.out.println(scanner2.nextLine());
+                Titan prueba = publicar_titan("prueba");
+                System.out.println("Nuevo titan publicado!");
+                System.out.println(prueba.getNombre_titan());
 
                 Thread t = new Thread(new CONNECTION(cs));
                 t.start();
+
+                //Publicar un titan --
             }
         } catch (IOException e){
             System.out.println(e.getMessage());
@@ -71,5 +88,28 @@ public class Distrit {
             //Logica servidor de distrito
 
         }
+    }
+
+    private static Titan publicar_titan(String nombre_distrito) {
+        Titan titan = new Titan();
+        String consola_distrito = "[Distrito "+ nombre_distrito +"]";
+        System.out.println(consola_distrito+" Publicar Titán");
+        System.out.println(consola_distrito+" Introducir nombre");
+        Scanner scanner2 = new Scanner(System.in);
+        String nombre_titan = scanner2.next();
+        titan.setNombre_titan(nombre_titan);
+        System.out.println(consola_distrito+" Introducir tipo");
+        System.out.println("1.- Normal");
+        System.out.println("2.- Excentrico");
+        System.out.println("3.- Cambiante");
+        titan.setTipo_titan(scanner2.nextInt());
+        System.out.println(consola_distrito+ " Se ha publicado el titan:"+ titan.getNombre_titan());
+        System.out.println("***************");
+        System.out.println("ID: "+titan.getID_titan());
+        System.out.println("Nombre: "+titan.getNombre_titan());
+        System.out.println("Tipo: " + titan.getTipo_titan());
+        System.out.print("***************");
+        return titan;
+        //Falta alertar el cliente!
     }
 }
